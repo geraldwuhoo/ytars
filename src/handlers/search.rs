b79d35aa9dca6350871257bdf3a7bb4a4a7d65fd
geraldwuhoo <gerald@geraldwu.com>
2023-12-06 23:33:32 -0800

    Make search a GET request instead of POST request for visible query parameters in the URL

--- src/handlers/search.rs
+++ src/handlers/search.rs
@@ -1,4 +1,8 @@
-use actix_web::{post, web, HttpResponse, Result};
+use actix_web::{
+    get,
+    http::header::{self, HeaderValue},
+    web, HttpResponse, Result,
+};
 use askama::Template;
 use log::debug;
 use serde::Deserialize;
@@ -13,40 +17,51 @@ use crate::structures::{
 #[template(path = "search.html")]
 struct SearchTemplate<'a> {
     videos: Vec<VideoChannelJoinModel>,
-    query: &'a str,
+    query: Option<&'a str>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct SearchParams {
-    query: String,
+    query: Option<String>,
 }
 
-#[post("/search")]
+#[get("/search")]
 pub async fn search_handler(
-    params: web::Form<SearchParams>,
+    params: web::Query<SearchParams>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, YtarsError> {
-    let query = &params.query;
-    debug!("Search query: {}", query);
-    let videos = sqlx::query_as!(
-        VideoChannelJoinModel,
-        r#"SELECT
-            video.id,
-            title,
-            upload_date,
-            duration_string,
-            channel.id AS channel_id,
-            channel.name,
-            video_type AS "video_type: VideoType"
-        FROM video
-        INNER JOIN channel ON video.channel_id = channel.id
-        WHERE document @@ plainto_tsquery($1)
-        ORDER BY ts_rank(document, plainto_tsquery($1)) DESC
-        LIMIT 100;"#,
-        query,
-    )
-    .fetch_all(pool.as_ref())
-    .await?;
+    let query = params.query.as_deref();
+    let videos = if let Some(query_str) = query {
+        if query_str.is_empty() {
+            debug!("Empty query string, redirecting to no query string URL");
+            return Ok(HttpResponse::PermanentRedirect()
+                .append_header((header::LOCATION, HeaderValue::from_static("/search")))
+                .finish());
+        }
+        debug!("Query string {} detected, querying database", query_str);
+        sqlx::query_as!(
+            VideoChannelJoinModel,
+            r#"SELECT
+                video.id,
+                title,
+                upload_date,
+                duration_string,
+                channel.id AS channel_id,
+                channel.name,
+                video_type AS "video_type: VideoType"
+            FROM video
+            INNER JOIN channel ON video.channel_id = channel.id
+            WHERE document @@ plainto_tsquery($1)
+            ORDER BY ts_rank(document, plainto_tsquery($1)) DESC
+            LIMIT 100;"#,
+            query_str,
+        )
+        .fetch_all(pool.as_ref())
+        .await?
+    } else {
+        debug!("No query string, skipping database query");
+        Vec::new()
+    };
 
     let search = SearchTemplate { videos, query };
     Ok(HttpResponse::Ok()
