ac19a47a70803cf7773eb423addaf13984aa5953
geraldwuhoo <gerald@geraldwu.com>
2023-12-08 22:19:52 -0800

    Select formats in yt-dlp

--- download.py
+++ download.py
@@ -59,6 +59,11 @@ for channel in data:
         "download_archive": f"{args.path}/archive",
         "progress": args.progress,
         "quiet": args.quiet,
+        # Download in priority:
+        # 1. webm compatible: 4320p > 2160p > 1440p > 1080p, AV1 > VP9, HFR preferred, OPUS
+        # 2. mp4 compatible: 1080p and lower, H264, M4A
+        # To avoid .mkv files (mainly h264 + opus), which cannot be played in the browser
+        "format": '(571/402/272/701/401/315/313/700/400/308/271/699/399/303/248)+bestaudio[acodec=opus]/bestvideo[vcodec^=avc1]+bestaudio[ext=m4a]',
     }
     with YoutubeDL(ydl_opts) as ydl:
         print(f"Downloading: {name} ({url})")
